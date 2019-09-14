0bbd281fa933302e5562ec6af66ba49061b2eac0
Ramesh Emandi <ramesh@quantana.in>
2019-09-14 11:12:10 +0530

    Fixed clip behavior and inkwell.

--- lib/views/bags.dart
+++ lib/views/bags.dart
@@ -33,46 +33,48 @@ class _BagsViewState extends State<BagsView> {
           return Container(
             padding: EdgeInsets.only(top: 10.0),
             child: Material(
-              child: InkWell(
-                splashColor: Colors.deepPurple,
-                onTap: (){
-                  DataApi().saveDataToFavoritesDataBase(bagsData[index]);
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                    content: Text('Added to Favorites'),
-                    duration: Duration(seconds: 3),
-                  ));
-//                  HomeRootWidget.tabsKey.currentState.tabController.animateTo(2);
-                },
-                child: new Card(
-
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(borderRadius:BorderRadius.all(Radius.circular(5.0))),
-                    elevation: 1.5,
-                    child: new Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      verticalDirection: VerticalDirection.down,
-                      children: <Widget>[
-                        Hero(
-                          tag: bagsData[index].title ,
-                          child: new Image.network(
-                            bagsData[index].image,
-                            height: 150.0,
-                            width: 100.0,),
-                        ),
-                        new Padding(
-                          padding: EdgeInsets.only(left: 10.0,top: 10.0),
-                          child: new Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              new Text(bagsData[index].title,style: TextStyle(color: Colors.black38,fontWeight: FontWeight.w700,fontSize: 18.0),),
-                              new Text(bagsData[index].price,style: TextStyle(color: Colors.black38,fontWeight: FontWeight.w500,fontSize: 15.0)),
-                            ],
+              child: new Card(
+                clipBehavior: Clip.hardEdge,
+                shape: RoundedRectangleBorder(borderRadius:BorderRadius.all(Radius.circular(5.0))),
+                elevation: 2.0,
+                child: InkWell(
+                  splashColor: Colors.purple,
+                  onTap: (){
+                    DataApi().saveDataToFavoritesDataBase(bagsData[index]);
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text('Added to Favorites'),
+                      duration: Duration(seconds: 3),
+                    ));
+                  },
+                  child: InkWell(
+                      splashColor: Colors.deepPurple,
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        verticalDirection: VerticalDirection.down,
+                        children: <Widget>[
+                          Hero(
+                            tag: bagsData[index].title ,
+                            child: new Image.network(
+                              bagsData[index].image,
+                              height: 150.0,
+                              width: 100.0,),
                           ),
-                        )
-                      ],
-                    )),
+                          new Padding(
+                            padding: EdgeInsets.only(left: 10.0,top: 10.0),
+                            child: new Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                new Text(bagsData[index].title,style: TextStyle(color: Colors.black38,fontWeight: FontWeight.w700,fontSize: 18.0),),
+                                new Text(bagsData[index].price,style: TextStyle(color: Colors.black38,fontWeight: FontWeight.w500,fontSize: 15.0)),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                ),
               ),
             ),
           );
